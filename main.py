ecc45af6a0dd401967185b321902be53358be02c
kiukiu7000 <kiukiuc7000@gmail.com>
2024-11-12 11:18:07 +0800

    adding new line to main.py in main

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+# Import necessary resources
 import math
 import numpy as np
 import pandas as pd
+import os
+#import tensorflow as tf
+#from tensorflow import keras
+#from tensorflow.keras.preprocessing.image import ImageDataGenerator
+#from torch.utils.data import DataLoader
+#from torchvision import transforms
+import json
+import matplotlib.pyplot as plt
 
 x = 10
 print(x)
